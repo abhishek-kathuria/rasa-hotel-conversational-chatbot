7c0dd94836c67d04a35d9a8368f3b59893ced3dd
Abhishek Kathuria <abhishekkathuria40@gmail.com>
2020-11-15 22:36:33 +0530

    final commit

--- actions/actions.py
+++ actions/actions.py
@@ -11,6 +11,33 @@
 #
 # from rasa_sdk import Action, Tracker
 # from rasa_sdk.executor import CollectingDispatcher
+
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action
+from rasa_sdk import Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import datetime
+
+class Action_Return_time(Action):
+   def name(self) -> Text:
+      return "action_return_time"
+
+   def run(self,
+           dispatcher: CollectingDispatcher,
+           tracker: Tracker,
+           domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+      tim=tracker.get_slot("time")
+      tim=int(tim)
+      now=datetime.datetime.now()
+      final_time=now.hour+tim
+      if(final_time>12):
+         dispatcher.utter_message("Sure, I have scheduled a cleaning for {} pm today".format(final_time-12))
+      else:
+         dispatcher.utter_message("Sure, I have scheduled a cleaning for {} am today".format(final_time))
+
+      return []
+
 #
 #
 # class ActionHelloWorld(Action):
